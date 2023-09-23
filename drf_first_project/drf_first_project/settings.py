ad3cb7c6e24431df003d931b30bcaa5aad93529d
Vladislav <vladkitov2003@mail.ru>
2023-09-23 21:11:18 +0300

    Реализовал корректное удаление записей. Упростил сериализатор с помощью встроенного класса.

--- drf_first_project/drf_first_project/settings.py
+++ drf_first_project/drf_first_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'women.apps.WomenConfig',
     'rest_framework',
+    'sequences.apps.SequencesConfig'
 ]
 
 MIDDLEWARE = [
--- drf_first_project/women/views.py
+++ drf_first_project/women/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Women
 from .serializers import WomenSerializer
+import sqlite3
 
 
 class WomenAPIView(APIView):
@@ -32,14 +33,20 @@ class WomenAPIView(APIView):
     def delete(self,request, *args, **kwargs):
         pk = kwargs.get('pk', None)
         if not pk:
-            return Response({'error': 'Method PUT not allowed'})
-
-        try:
-            instance = Women.objects.get(pk=pk)
-        except Exception:
             return Response({'error': 'Object does not exist'})
 
+        try:
+            instance = Women.objects.filter(id=pk)
+        except Exception:
+            return Response({'error': 'Object does not exist'})
         instance.delete()
+        connection = sqlite3.connect('db.sqlite3')
+        cursor = connection.cursor()
+        cursor.execute('SELECT MAX(id) FROM women_women')
+        last_id = cursor.fetchall()[0][0]
+        cursor.execute("UPDATE SQLITE_SEQUENCE SET SEQ=?", (int(last_id),))
+        connection.commit()
+        connection.close()
         return Response({'post': f"delete post: {str(pk)}"})
 
 
